b9cc3f83c828b9016111865901435689cce08d1a
Marina Sysoliatina <stereogam@yandex.ru>
2024-08-07 21:06:53 +0200

    remove template null

--- src/components/burger-ingredients/burger-ingredients.tsx
+++ src/components/burger-ingredients/burger-ingredients.tsx
@@ -55,7 +55,7 @@ export const BurgerIngredients: FC = () => {
       titleSaucesRef.current?.scrollIntoView({ behavior: 'smooth' });
   };
 
-  return null;
+  // return null;
 
   return (
     <BurgerIngredientsUI
