103b6a7ba2935b6c7801eea156748c921310fb5b
Marina Sysoliatina <stereogam@yandex.ru>
2025-02-17 00:25:40 +0100

    test env

--- src/utils/burger-api.ts
+++ src/utils/burger-api.ts
@@ -1,7 +1,8 @@
 import { setCookie, getCookie } from './cookie';
 import { TIngredient, TOrder, TOrdersData, TUser } from './types';
 
-const URL = process.env.BURGER_API_URL;
+const URL = process.env.BURGER_API_URL
+console.log(process.env.BURGER_API_URL);
 
 const checkResponse = <T>(res: Response): Promise<T> =>
   res.ok ? res.json() : res.json().then((err) => Promise.reject(err));
