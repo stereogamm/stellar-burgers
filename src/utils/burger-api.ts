30f871618cf7221e3dded601b9bb939525720ef5
Marina Sysoliatina <stereogam@yandex.ru>
2025-02-17 11:57:18 +0100

    remove env file for prod deploy

--- src/utils/burger-api.ts
+++ src/utils/burger-api.ts
@@ -1,7 +1,7 @@
 import { setCookie, getCookie } from './cookie';
 import { TIngredient, TOrder, TOrdersData, TUser } from './types';
 
-const URL = process.env.BURGER_API_URL
+const URL = process.env.BURGER_API_URL;
 console.log(process.env.BURGER_API_URL);
 
 const checkResponse = <T>(res: Response): Promise<T> =>
